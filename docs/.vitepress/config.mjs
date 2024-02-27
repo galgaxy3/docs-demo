9d4eac35d1ba9396583a3752adfe35c8ed359fea
zhaoxuyan <zhaoxuyan@inspur.com>
2024-02-27 15:32:01 +0800

    algolia test

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -76,30 +76,39 @@ export default defineConfig({
 
 
     // 设置搜索框的样式
-    search: {
-      provider: "local",
+    // search: {
+    //   provider: "local",
+    //   options: {
+    //     locales: {
+    //       zh: {
+    //         translations: {
+    //           button: {
+    //             buttonText: '搜索文档',
+    //             buttonAriaLabel: '搜索文档'
+    //           },
+    //           modal: {
+    //             noResultsText: '无法找到相关结果',
+    //             resetButtonTitle: '清除查询条件',
+    //             footer: {
+    //               selectText: '选择',
+    //               navigateText: '切换'
+    //             }
+    //           }
+    //         }
+    //       }
+    //     },
+    //   },
+    // },
+    search:{
+      provider: 'algolia',
       options: {
-        locales: {
-          zh: {
-            translations: {
-              button: {
-                buttonText: '搜索文档',
-                buttonAriaLabel: '搜索文档'
-              },
-              modal: {
-                noResultsText: '无法找到相关结果',
-                resetButtonTitle: '清除查询条件',
-                footer: {
-                  selectText: '选择',
-                  navigateText: '切换'
-                }
-              }
-            }
-          }
-        },
-      },
+        appId: 'WSR1YMJZCS',
+        apiKey: 'f99424b3f822f6c6767254e63a95d6f7',
+        indexName: 'docs-demo',
+        placeholder: '请输入关键词',
+        buttonText: '搜索',
+      }
     },
-
 
   },
   // vite:{
